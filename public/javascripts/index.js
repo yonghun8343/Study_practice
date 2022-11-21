1dd3d3933d23ae4fed92a27a328c94d0fbc5dd85
hun <yonghun83@naver.com>
2022-11-21 14:51:12 +0900

    추가부분 업로드

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -60,25 +60,6 @@ document.getElementById("find_submit").addEventListener("click", () => {
     document.getElementById("find_pwd").style.visibility = "visible";
     submitBtn.style.transform = "translateY(0px)";
   }
-
-  if (document.getElementById("find_pwd").value) {
-    const xhr = new XMLHttpRequest();
-    const data = {
-      email: document.getElementById("find_email").value,
-      pwd: document.getElementById("find_pwd").value,
-    };
-    xhr.onload = () => {
-      if (xhr.status === 200 || xhr.status === 201) {
-        console.log(xhr.responseText); // 서버에서 보내주는 값
-      }
-    };
-    xhr.onerror = () => {
-      console.error(xhr.responseText);
-    };
-    xhr.open("PUT", "http://localhost:3000/users/changePwd");
-    xhr.setRequestHeader("Content-Type", "application/json"); // 콘텐츠 타입을 json으로 설정
-    xhr.send(JSON.stringify(data)); // 데이터를 포함하여 전송
-  }
 });
 
 document.getElementById("sign-up").addEventListener("click", () => {
@@ -134,26 +115,75 @@ document.getElementById("signup-submit").addEventListener("click", () => {
 
   submitBtn.style.transition = "transform 1s linear";
 
-  if (document.getElementById("sign_email").value) {
-    document.getElementById("sign_valid").style.visibility = "visible";
-    submitBtn.style.transform = "translateY(-240px)";
-    document.getElementById("sign_email").disabled = true;
+  if (
+    document.getElementById("sign_email").value &&
+    document.getElementById("sign_email").disabled === false
+  ) {
+    const xhr = new XMLHttpRequest();
+    const data = {
+      email: document.getElementById("sign_email").value,
+    };
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 201) {
+          document.getElementById("sign_valid").style.visibility = "visible";
+          submitBtn.style.transform = "translateY(-240px)";
+          document.getElementById("sign_email").disabled = true;
+        } else {
+          document.getElementById("sign-text").innerText =
+            "잠시 후 다시 시도 해 주세요.";
+          document.getElementById("sign-text").style.color = "red";
+        }
+      }
+    };
+    xhr.open("POST", "http://localhost:3000/users/auth_mail");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
   }
 
-  if (document.getElementById("sign_valid").value.length === 6) {
-    document.getElementById("sign_pw").style.visibility = "visible";
-    submitBtn.style.transform = "translateY(-175px)";
-    document.getElementById("sign_valid").disabled = true;
+  if (
+    document.getElementById("sign_valid").value.length === 6 &&
+    document.getElementById("sign_valid").disabled === false
+  ) {
+    const xhr = new XMLHttpRequest();
 
-    setTimeout(() => {
-      document.getElementById("sign_name").style.visibility = "visible";
-      submitBtn.style.transform = "translateY(-115px)";
-    }, 900);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        if (response.status === "success") {
+          document.getElementById("sign_pw").style.visibility = "visible";
+          submitBtn.style.transform = "translateY(-175px)";
+          document.getElementById("sign_valid").disabled = true;
 
-    setTimeout(() => {
-      document.getElementById("sign_nick").style.visibility = "visible";
-      submitBtn.style.transform = "translateY(-25px)";
-    }, 1800);
+          setTimeout(() => {
+            document.getElementById("sign_name").style.visibility = "visible";
+            submitBtn.style.transform = "translateY(-115px)";
+          }, 900);
+
+          setTimeout(() => {
+            document.getElementById("sign_nick").style.visibility = "visible";
+            submitBtn.style.transform = "translateY(-25px)";
+          }, 1800);
+        } else {
+          document.getElementById("sign-text").innerText = "일치하지 않습니다.";
+        }
+      }
+    };
+
+    xhr.onerror = () => {
+      document.getElementById("sign-text").innerText =
+        "잠시 후 다시 시도 해 주세요.";
+      console.error(xhr.responseText);
+    };
+
+    xhr.open(
+      "GET",
+      `http://localhost:3000/users/auth_valid?email=${
+        document.getElementById("sign_email").value
+      }&digit=${document.getElementById("sign_valid").value}`
+    );
+    xhr.send();
   }
 
   if (
@@ -169,17 +199,32 @@ document.getElementById("signup-submit").addEventListener("click", () => {
       name: document.getElementById("sign_name").value,
       nick: document.getElementById("sign_nick").value,
     };
+
     xhr.onload = () => {
-      if (xhr.status === 200 || xhr.status === 201) {
-        console.log(xhr.responseText); // 서버에서 보내주는 값
+      if (xhr.status === 200) {
+        // 이미 가입된 이메일이 있음.
+        document.getElementById("sign-text").innerText =
+          "이미 가입된 이메일이 있습니다.";
+      } else if (xhr.status === 201) {
+        // 성공적으로 가입 되었을 때 최초 화면으로 변경
+        // location.replace와 location.href 다른점 참고
+        // location.replace는 히스토리에 남지 않음
+        // location.href는 히스토리에 남음
+        location.replace("http://localhost:3000");
+        console.log(xhr.responseText);
+      } else {
+        // status가 200, 201이 아닐 때 (400)일 때
+        document.getElementById("sign-text").innerText =
+          "오류가 발생 했습니다.";
       }
     };
     xhr.onerror = () => {
       console.error(xhr.responseText);
     };
+
     xhr.open("POST", "http://localhost:3000/users/register");
-    xhr.setRequestHeader("Content-Type", "application/json"); // 콘텐츠 타입을 json으로 설정
-    xhr.send(JSON.stringify(data)); // 데이터를 포함하여 전송
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
   }
 });
 
@@ -189,15 +234,24 @@ document.getElementById("login_btn").addEventListener("click", () => {
     email: document.getElementById("id").value,
     pwd: document.getElementById("pw").value,
   };
+
   xhr.onload = () => {
-    if (xhr.status === 200 || xhr.status === 201) {
-      console.log(xhr.responseText); // 서버에서 보내주는 값
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      if (response.status === "success") {
+        document.getElementById("login-text").innerText = "로그인 성공";
+      } else {
+        // response.status === "fail
+        document.getElementById("login-text").innerText = response.message;
+      }
     }
   };
+
   xhr.onerror = () => {
     console.error(xhr.responseText);
   };
+
   xhr.open("POST", "http://localhost:3000/users/login");
-  xhr.setRequestHeader("Content-Type", "application/json"); // 콘텐츠 타입을 json으로 설정
-  xhr.send(JSON.stringify(data)); // 데이터를 포함하여 전송
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send(JSON.stringify(data));
 });
--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,15 @@ router.post("/auth_mail", (req, res) => {
 
   asyncSQL(
     `INSERT INTO auth (a_email, a_digit) VALUES ("${email}", "${rnd}";)`,
-    (err1, rows) => {
-      console.log(err1);
-      console.log(rows.affectedRows);
-      if (err1 || rows.affectedRows < 1) {
+    (err, rows) => {
+      if (err || rows.affectedRows < 1) {
         res.status(500).json({
           status: "fail",
           message: "서버에서 에러가 발생 하였습니다.",
         });
       } else {
-        sendMail(email, rnd, (err2, info) => {
-          console.log(err2);
-          console.log(info);
-          if (err2) {
+        sendMail(email, rnd, (err1) => {
+          if (err1) {
             res.status(500).json({
               status: "fail",
               message: "서버에서 에러가 발생 하였습니다.",
@@ -61,30 +57,32 @@ router.post("/auth_mail", (req, res) => {
 
 router.get("/auth_valid", (req, res) => {
   const { email, digit } = req.query;
-
   asyncSQL(
-    `SELECT a_id, a_digit FROM auth WHERE a_email = "${email}" AND a_is_used = 0 ORDER BY a_id DESC LIMIT 1;`,
+    `SELECT a_id, a_digit FROM auth WHERE a_email = "${email}" AND a_is_used = 0 ORDER BY a_id DESC LIMIT 1`,
     (err, rows) => {
-      if (rows.length > 0) {
-        if (digit.toString() === rows[0].a_digit.toString()) {
-          asyncSQL(
-            `UPDATE auth SET a_is_used = 1 WHERE a_id = "${rows[0].a_id}"`
-          );
-          res.status(200).json({
-            status: "success",
-            message: "일치 합니다.",
-          });
-        } else {
-          res.status(200).json({
-            status: "fail",
-            message: "일치하지 않습니다.",
-          });
-        }
+      if (err || rows.length < 1) {
+        res
+          .status(500)
+          .json({ status: "fail", message: "서버에서 오류가 발생했습니다." });
+      } else if (digit.toString() === rows[0].a_digit.toString()) {
+        asyncSQL(
+          `UPDATE auth SET a_is_used = 1 WHERE a_id = ${rows[0].a_id}`,
+          (err1, rows1) => {
+            if (err1 || rows1.affectedRows < 1) {
+              res.status(500).json({
+                status: "fail",
+                message: "서버에서 에러가 발생했습니다.",
+              });
+            } else {
+              res.status(200).json({
+                status: "sucsess",
+                message: "일치합니다.",
+              });
+            }
+          }
+        );
       } else {
-        res.status(200).json({
-          status: "fail",
-          message: "이메일이 존재하지 않습니다.",
-        });
+        res.status(200).json({ status: "fail", message: "일치하지 않습니다." });
       }
     }
   );
@@ -118,18 +116,35 @@ router.post("/register", (req, res) => {
   const encryptPwd = encrypt(pwd);
 
   asyncSQL(
-    `INSERT INTO user (u_email, u_pwd, u_name, u_nick) VALUES ("${email}", "${encryptPwd}", "${name}", "${nick}");`,
+    `SELECT u_email FROM user WHERE u_email = "${email}"`,
     (err, rows) => {
-      if (err || rows.affectedRows < 1) {
+      if (err) {
         res.status(500).json({
           status: "fail",
           message: "서버에서 에러가 발생 하였습니다.",
         });
-      } else {
-        res.status(201).json({
-          status: "success",
-          message: "성공되었습니다.",
+      } else if (rows.length > 0) {
+        res.status(200).json({
+          status: "fail",
+          message: "이미 가입된 이메일이 있습니다.",
         });
+      } else {
+        asyncSQL(
+          `INSERT INTO user (u_email, u_pwd, u_name, u_nick) VALUES ("${email}", "${encryptPwd}", "${name}", "${nick}";)`,
+          (err1, rows1) => {
+            if (err1 || rows1.affectedRows < 1) {
+              res.status(500).json({
+                status: "fail",
+                message: "서버에서 에러가 발생 하였습니다.",
+              });
+            } else {
+              res.status(201).json({
+                status: "success",
+                message: "성공되었습니다.",
+              });
+            }
+          }
+        );
       }
     }
   );
@@ -137,13 +152,11 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   const { email, pwd } = req.body;
-  console.log(email, pwd);
   const encryptPwd = encrypt(pwd);
 
   asyncSQL(
     `SELECT u_pwd FROM user WHERE u_email = "${email}";`,
     (err, rows) => {
-      console.log(rows);
       if (err) {
         res.status(500).json({
           status: "fail",
@@ -176,35 +189,60 @@ router.post("/login", (req, res) => {
 // 임시 비밀번호 발급
 router.put("/changePwd", (req, res) => {
   const { email, pwd } = req.body;
-  if (!email && !pwd) {
+  if (!email || !pwd) {
+    // 해당 값이 제대로 존재 하는지 다시 확인
     res.status(400).json({
       status: "fail",
     });
   }
   const encryptPwd = encrypt(pwd);
-
-  asyncSQL(
-    `UPDATE user SET u_pwd = "${encryptPwd}" WHERE u_email = "${email}";`,
-    (err, rows) => {
-      console.log(rows);
-      if (err) {
-        res.status(500).json({
-          status: "fail",
-          message: "서버에서 에러가 발생 하였습니다.",
-        });
-      } else if (rows.affectedRows > 0) {
+  // 비밀번호 변경하기.
+  // 1. 에러가 발생 할 때
+  // 2. 기존 비밀번호랑 일치하는지
+  // 3. 이메일을 찾을 수 없을 때
+  asyncSQL(`SELECT u_pwd FROM user WHERE u_email = "${email}"`, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "서버에서 에러가 발생 하였습니다.",
+      });
+    }
+    if (rows.length > 0) {
+      if (encryptPwd === rows[0].u_pwd) {
         res.status(200).json({
-          status: "success",
-          message: "성공적으로 바뀌었습니다.",
+          status: "fail",
+          message: "기존 비밀번호와 일치 합니다.",
         });
       } else {
-        res.status(200).json({
-          status: "fail",
-          message: "이메일을 찾을 수 없습니다.",
-        });
+        asyncSQL(
+          `UPDATE user SET u_pwd = "${encryptPwd}" WHERE u_email = ${email}`,
+          (err1, rows1) => {
+            if (err1) {
+              res.status(500).json({
+                status: "fail",
+                message: "서버에서 에러가 발생 하였습니다.",
+              });
+            } else if (rows1.affectedRows > 0) {
+              res.status(200).json({
+                status: "success",
+                message: "성공적으로 바뀌었습니다.",
+              });
+            } else {
+              res.status(200).json({
+                status: "fail",
+                message: "이메일을 찾을 수 없습니다.",
+              });
+            }
+          }
+        );
       }
+    } else {
+      res.status(200).json({
+        status: "fail",
+        message: "이메일을 찾을 수 없습니다.",
+      });
     }
-  );
+  });
 });
 
 module.exports = router;
