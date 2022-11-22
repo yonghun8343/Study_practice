0eff902f95b4187c565bcecc3641a9566872e48f
yonghun8343 <yonghun83@naver.com>
2022-11-23 00:30:44 +0900

    add 221124 수업 할 내용

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 
-function makeComment(name, date, content) {
+function makeBoard(bid, name, date, content) {
   const div1 = document.createElement("div");
   div1.className = "board-content";
 
@@ -27,9 +27,49 @@ function makeComment(name, date, content) {
   span1_3_1_1.innerText = "댓글 보기";
   div1_3_1.append(span1_3_1_1);
 
+  div1_3_1.addEventListener("click", () => {
+    location.href(`http://localhost:3000/detail.html/${bid}`);
+  });
+
   div1_3.append(div1_3_1);
 
   div1.append(div1_1, div1_2, div1_3);
 
   document.getElementsByClassName("wrap-right-bottom")[0].appendChild(div1);
 }
+
+function getDateTime(date) {
+  const dt = new Date(date);
+  const year = dt.getFullYear();
+  const month = `0${dt.getMonth() + 1}`.slice(-2);
+  const day = `0${dt.getDate()}`.slice(-2);
+  const hh = `0${dt.getHours()}`.slice(-2);
+  const mm = `0${dt.getMinutes()}`.slice(-2);
+  const ss = `0${dt.getSeconds()}`.slice(-2);
+  // 몇 년전, 몇 분전, 몇 초전 구현 가능?
+  // ISO 시간 써서 switch문으로 구현
+  return `${year}-${month}-${day} ${hh}:${mm}:${ss}`;
+}
+
+console.log("111");
+
+const xhr = new XMLHttpRequest();
+xhr.onload = () => {
+  console.log("222");
+  if (xhr.status === 200) {
+    console.log("333");
+    const response = JSON.parse(xhr.responseText);
+    console.log(response);
+    response.content.forEach((element) => {
+      makeBoard(
+        element.bid,
+        element.nick,
+        getDateTime(element.date),
+        element.content
+      );
+    });
+  }
+};
+
+xhr.open("GET", `http://localhost:3000/board/get/1?limit=10`);
+xhr.send();
--- routes/board.js
+++ routes/board.js
@@ -38,17 +38,33 @@ router.post("/write", (req, res) => {
 // http://localhost:3000/boadrd/get/1
 router.get("/get/:uid", (req, res) => {
   const { uid } = req.params;
+  let { count } = req.query;
+  if (!uid) {
+    res.status(400).end();
+  }
+  if (!count) {
+    count = 10;
+  }
+  // 삼항 연산자
+  // let count = !req.query.count ? 10 : req.query.count;
+  // let req = {
+  //   query: {
+  //       count: 1
+  //   }
+  // } << 이걸로 예시
 
+  // bid추가
   asyncSQL(
     `SELECT 
       a.u_nick as nick,
+      b.b_id as bid,
       b.b_content as content,
       b.b_date as date
     FROM board b JOIN user a
     ON b.b_uid = a.u_id
     WHERE b_uid = "${uid}"
     ORDER BY b_date DESC
-    LIMIT 10`,
+    LIMIT ${count}`,
     (err, rows) => {
       if (err) {
         res.status(500).json({
@@ -62,7 +78,6 @@ router.get("/get/:uid", (req, res) => {
         // 여러개의 값을 처리를 할 필요가 있음.
         // 10개에 대한 데이터를 json안에다가 넣어 주어야 되죠 왜냐 rows = []
         // 우다다다 넣는 방법
-        console.log(rows);
         res.status(200).json({
           status: "success",
           content: rows,
@@ -71,5 +86,185 @@ router.get("/get/:uid", (req, res) => {
     }
   );
 });
+
+// 팔로워 한 사람들 글 조회
+router.get("/get/all/:uid", (req, res) => {
+  const { uid } = req.params;
+  let { count } = req.query;
+  if (!uid) {
+    res.status(400).end();
+  }
+  if (!count) {
+    count = 10;
+  }
+
+  asyncSQL(
+    `SELECT
+      a.u_nick as nick,
+      b.b_content as content,
+      b.b_date as date
+    FROM board b JOIN user a
+    ON b.b_uid = a.u_id
+    WHERE b.b_uid IN (SELECT f_ing FROM follow WHERE f_er = ${uid})
+    ORDER BY b_date DESC
+    LIMIT ${count}`,
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({
+          status: "fail",
+          message: "서버에서 에러가 발생 하였습니다.",
+        });
+        if (process.env.NODE_ENV === "development") {
+          console.error(err);
+        }
+      } else {
+        res.status(200).json({
+          status: "success",
+          content: rows,
+        });
+      }
+    }
+  );
+});
+
+// 특정 개수 조회했는데 10개 안되서 부족한거 채울 때
+// not을 써서 해당 유저 이외 조회
+
+router.get("/get/any/:uid", (req, res) => {
+  const { uid } = req.params;
+
+  asyncSQL(
+    `SELECT
+      a.u_nick as nick,
+      b.b_content as content,
+      b.b_date as date
+    FROM board b JOIN user a
+    ON b.b_uid = a.u_id
+    WHERE b.b_uid NOT IN (SELECT f_ing FROM follow WHERE f_er = ${uid})
+    ORDER BY b_date DESC
+    LIMIT 10`,
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({
+          status: "fail",
+          message: "서버에서 에러가 발생 하였습니다.",
+        });
+        if (process.env.NODE_ENV === "development") {
+          console.error(err);
+        }
+      } else {
+        res.status(200).json({
+          status: "success",
+          content: rows,
+        });
+      }
+    }
+  );
+});
+
+// 글 수정
+// 진짜 글 작성한 사람이 맞는지 확인
+router.put("/fix/:bid", (req, res) => {
+  const { bid } = req.params;
+  const { content, uid } = req.body;
+
+  asyncSQL(`SELECT b_uid FROM board WHERE b_id = "${bid}"`, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "서버에서 에러가 발생 하였습니다.",
+      });
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
+    } else if (rows.length > 0) {
+      if (rows[0].b_uid === Number(uid)) {
+        asyncSQL(
+          `UPDATE board SET b_content = "${content}" WHERE b_id = "${bid}"`,
+          (err1, rows1) => {
+            if (err1) {
+              res.status(500).json({
+                status: "fail",
+                message: "서버에서 에러가 발생 하였습니다.",
+              });
+              if (process.env.NODE_ENV === "development") {
+                console.error(err);
+              }
+            } else if (rows1.affectedRows > 0) {
+              res.status(200).json({
+                status: "success",
+                message: "성공적으로 바뀌었습니다.",
+              });
+            } else {
+              res.status(200).json({
+                status: "fail",
+                message: "정상적으로 수행을 하지 못하였습니다.",
+              });
+            }
+          }
+        );
+      } else {
+        res.status(403).end();
+      }
+    } else {
+      res.status(403).end();
+    }
+  });
+});
+
+// 글 삭제
+router.delete("/delete/:bid", (req, res) => {
+  // const bid = req.params.bid
+  const { bid } = req.params;
+  const { uid } = req.body;
+
+  asyncSQL(`SELECT b_uid FROM board WHERE b_id = "${bid}"`, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "서버에서 에러가 발생 하였습니다.",
+      });
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
+    } else if (rows.length > 0) {
+      if (rows[0].b_uid === Number(uid)) {
+        asyncSQL(`DELETE FROM board WHERE b_id = "${bid}"`, (err1, rows1) => {
+          if (err1) {
+            res.status(500).json({
+              status: "fail",
+              message: "서버에서 에러가 발생 하였습니다.",
+            });
+            if (process.env.NODE_ENV === "development") {
+              console.error(err);
+            }
+          } else if (rows1.affectedRows > 0) {
+            res.status(200).json({
+              status: "success",
+              message: "성공적으로 바뀌었습니다.",
+            });
+          } else {
+            res.status(200).json({
+              status: "fail",
+              message: "정상적으로 수행을 하지 못하였습니다.",
+            });
+          }
+        });
+      } else {
+        res.status(403).end();
+      }
+    } else {
+      res.status(403).end();
+    }
+  });
+});
+
+// 댓글 조회
+
+// 댓글 작성
+
+// 댓글 수정
+
+// 댓글 삭제
 
 module.exports = router;
